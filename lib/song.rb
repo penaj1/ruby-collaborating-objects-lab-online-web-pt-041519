41030e014dd75c5f278b74ce1ff4916b26c3b167
Janice Pena <jipena94@gmail.com>
2019-05-25 02:15:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+    @artist = artist
+  end
+   
+  def self.new_by_filename(filename)
+    artist_name = filename.split("-")[0]
+    song_name = filename.split("-")[1].strip
+    song = Song.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    song.artist = artist
+    artist.add_song(song)
+    return song
+  end
+
+end
