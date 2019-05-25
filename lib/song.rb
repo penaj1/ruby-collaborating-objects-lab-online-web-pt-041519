0d22ca06154c848c0be93559821350af1723fcde
Janice Pena <jipena94@gmail.com>
2019-05-25 02:18:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   end
    
   def self.new_by_filename(filename)
-    artist_name = filename.split("-")[0]
+    artist_name = filename.split(" - ")[0]
     song_name = filename.split("-")[1].strip
     song = Song.new(song_name)
     artist = Artist.find_or_create_by_name(artist_name)
