60548cffe9c21a5805c89f6553f2fda3607e3fa7
Janice Pena <jipena94@gmail.com>
2019-05-25 02:19:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split("-")[1].strip
     song = Song.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
     #song.artist = artist
     #artist.add_song(song)
     return song
