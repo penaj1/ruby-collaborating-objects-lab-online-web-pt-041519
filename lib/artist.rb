53741e71a6d91c3f31c858c8535d89db9eeb3737
Janice Pena <jipena94@gmail.com>
2019-05-25 02:29:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist 
+  
+  attr_accessor :name
+  attr_reader :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+ 
+  def add_song(song)
+    @songs << song
+  end
+  
+  def self.find_or_create_by_name(name)
+    artist = @@all.find {|artist| artist.name == name}
+    if artist == nil
+      artist = Artist.new(name)
+      artist.save
+    end
+    artist
+  end
+
+  def print_songs()
+    puts @songs.collect{|x| x.name}
+  end
+  
+end
